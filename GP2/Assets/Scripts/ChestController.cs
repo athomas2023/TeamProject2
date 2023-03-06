571338dc4ad525d752de57dfed0fb2d639286f89
athomas2023 <97468398+athomas2023@users.noreply.github.com>
2023-03-05 20:25:10 -0500

    Final Push

--- GP2/Assets/Scripts/ChestController.cs
+++ GP2/Assets/Scripts/ChestController.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class ChestController : MonoBehaviour
 {
     public GameObject winCanvas;
+    Animator animator;
+
+    public float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 
         void OnTriggerEnter(Collider other)
     {
@@ -23,10 +22,20 @@ public class ChestController : MonoBehaviour
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
         if (other.gameObject.tag == "Player")
         {
-            
+            animator.SetTrigger("Play");
             winCanvas.SetActive(true);
             Debug.Log("win!");
-            Time.timeScale = 0f;
+            StartCoroutine(Win());
         }
+    }
+
+
+      IEnumerator Win()
+    {
+        
+        yield return new WaitForSeconds(time);
+         Time.timeScale = 0f;
+        
+    
     }
 }
